c903482bfd633afd24dd11571483eeda7df86637
HarryPaterson <harry.c.paterson@gmail.com>
2023-06-14 17:26:48 +0100

    confirm_start

--- myers_briggs_quiz.py
+++ myers_briggs_quiz.py
@@ -5,7 +5,13 @@ print("Welcome to the Myers-Briggs personality test!")
 print("Would you like to know what personality type you are?")
 
 # Confirm start
+yes = input("Enter 'yes' to start the quiz: ")
 
+if yes == "yes":
+    print("Lets begin!")
+else:
+    print("Ok suit yourself.")
+    quit()
 
 # Set type statements
 
