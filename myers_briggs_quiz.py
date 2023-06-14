17c188a939ab4babab482fd4505d309326f94fc0
HarryPaterson <harry.c.paterson@gmail.com>
2023-06-14 17:22:58 +0100

    welcome_messages

---
+++ myers_briggs_quiz.py
@@ -0,0 +1,18 @@
+# Myers-Briggs type quiz
+
+# Welcome messages
+print("Welcome to the Myers-Briggs personality test!")
+print("Would you like to know what personality type you are?")
+
+# Confirm start
+
+
+# Set type statements
+
+# Set type variables
+
+# Calculate result
+
+# Result
+
+#Bonus again
